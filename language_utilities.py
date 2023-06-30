46f973844a455f0ed0620c1621f9061a9fc9ad7d
Anton Chaplygin <antoncp@gmail.com>
2023-07-01 00:00:43 +0200

    Update language_utilities.py for 11-19 cases

--- language_utilities.py
+++ language_utilities.py
@@ -1,7 +1,8 @@
-def choose_noun_case(score, word):
+def choose_noun_case(score, word='балл'):
     ending = ''
     remainder = score % 10
-    if remainder >= 5 or remainder == 0:
+    if (remainder >= 5 or remainder == 0 or
+       (score > 10 and 10 < int(str(score)[-2:]) < 20)):
         ending = 'ов'
     elif 1 < remainder < 5:
         ending = 'a'
