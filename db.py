9feb35cb1928ddaa65883514fa2f3f20e16a6bc2
antoncp <antoncp@gmail.com>
2023-07-09 19:51:42 +0200

    New test table in a DB

--- db.py
+++ db.py
@@ -19,6 +19,12 @@ class DataBase:
                 faculty TEXT,
                 points INTEGER
             );
+            CREATE TABLE IF NOT EXISTS test_scores(
+                id INTEGER PRIMARY KEY,
+                time TEXT,
+                faculty TEXT,
+                points INTEGER
+            );
             '''
             )
         return True
@@ -41,6 +47,30 @@ class DataBase:
                 '''
             SELECT faculty, SUM(points) AS score
             FROM scores
+            GROUP BY faculty
+            ORDER BY score DESC;
+            '''
+            )
+        return self.cursor.fetchall()
+
+    def test_save_points(self, faculty, points):
+        """Saves points associated with faculty"""
+        with self.connection:
+            self.cursor.execute(
+                '''
+                INSERT INTO test_scores (time, faculty, points)
+                VALUES(datetime('now'), ?, ?);
+                ''',
+                (faculty, points),
+            )
+
+    def test_get_all_points(self):
+        """Fetches all score summary"""
+        with self.connection:
+            self.cursor.execute(
+                '''
+            SELECT faculty, SUM(points) AS score
+            FROM test_scores
             GROUP BY faculty
             ORDER BY score DESC;
             '''
