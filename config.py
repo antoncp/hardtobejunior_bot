c2c004b413875b5c879bf72995487143f2f0d1c0
antoncp <antoncp@gmail.com>
2024-01-24 23:03:34 +0100

    Fixes variable loading error

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Settings:
     DEBUG = os.getenv("DEBUG") == "True"
     ADMIN_ID = int(os.environ.get("ADMIN_ID"))
     INSPECT_ID = int(os.environ.get("INSPECT_ID"))
-    CHAT_ALERT = int(os.environ.get("CHAT_ALERT"))
+    CHAT_ALERT = int(os.environ.get("CHAT_ALERT", 12345))
     YA_ID = os.environ.get("YA_ID")
     API_LOGIN = os.environ.get("API_LOGIN")
     API_PAS = os.environ.get("API_PAS")
